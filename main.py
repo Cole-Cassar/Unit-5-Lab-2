1ebcb61ad67329189c4b9e6fe4af908049957436
Cole-Cassar <cole.cassar@edu.sait.ca>
2023-10-23 15:53:32 -0600

    Adding comments

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 file = open("sample.ini")
-
+# Opens up file "sample.ini"
 num_words = 0
-
+# Initial counter
 with open("sample.ini") as file:
     for line in file:
-        
+        # Process which takes the file and reads each line
         words = line.split()
         num_words += len(words)
-
+# Reads all "words" and produces the total number
 print("Number of words: ")
 print(num_words)
-
+# Shows how many words were in the file
 file = open("counts.txt", "w")
 file.write(f"Total number of words:\n {num_words}")
 file.close
+# Creating "counts.txt", this file will show the amount of words in the "sample.ini" file
